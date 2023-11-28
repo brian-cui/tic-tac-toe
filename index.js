8871afda420fbca7ff2b311bc2456000d2cfeffc
brian-cui <73263642+brian-cui@users.noreply.github.com>
2023-11-27 19:41:22 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('It is alive');
+// Tic Tac Toe
+
+// Set up the board
+    // Track the current player (x,o)
+    // Track playcount
+    // create 9 boxes and append to the board element
+        // boxes need a click listener
+            // check to make sure the box is not taken
+            // add the value for the current player (x,o)
+            // possibly remove click event listener
+            // check for win
+            // check that gameCount is < 9
+            // update the current player
+            // update a message on the screen so you know who's turn it is
+            
+//Take turn
+  //
+
+//Check for win (only necessary when playcount hits 5)
+    //if someone wins display message
+
+    //if not a win, switch player
+
+
+
+function startGame() {}
+
+function createBoxes() {}
+
+function checkForWin() {}
